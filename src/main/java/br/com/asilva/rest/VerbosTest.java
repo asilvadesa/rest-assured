000e33bd00d4acd011810209781c1418a94902ca
Anderson de Sa <asilvadesa@gmail.com>
2020-11-21 21:00:34 -0400

    Add caso de teste usuario sem atributo name.

--- src/main/java/br/com/asilva/rest/VerbosTest.java
+++ src/main/java/br/com/asilva/rest/VerbosTest.java
@@ -3,8 +3,7 @@ package br.com.asilva.rest;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 public class VerbosTest {
 
@@ -24,5 +23,22 @@ public class VerbosTest {
             .body("age", is(50))
         ;
     }
+
+    @Test
+    public void naoDeveSalvarUsuarioSemNome(){
+        given()
+                .log().all()
+                .contentType("application/json")
+                .body("{\"age\": 50}")
+        .when()
+                .post("https://restapi.wcaquino.me/users")
+        .then()
+                .log().all()
+                .statusCode(400)
+                .body("id", is(nullValue()))
+                .body("error", is("Name é um atributo obrigatório"));
+
+    }
+
 }
 
