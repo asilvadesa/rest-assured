b0ae5bd9cdfaf6add0151332b170a023898f8e75
Anderson de Sa <asilvadesa@gmail.com>
2020-12-06 10:24:11 -0400

    Validando download e upload de arquivos

---
+++ src/main/java/br/com/asilva/rest/FileTest.java
@@ -0,0 +1,60 @@
+package br.com.asilva.rest;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.*;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
+
+public class FileTest {
+
+    @Test
+    public void deveObrigarEnvioArquivo(){
+
+        given().log().all()
+
+                .when()
+                    .post("https://restapi.wcaquino.me/upload")
+                .then()
+                    .log().all()
+                    .statusCode(404)
+                    .body("error", is("Arquivo não enviado"));
+        ;
+    }
+
+    @Test
+    public void deveOFazerUploadArquivo(){
+
+        given().log().all()
+                .multiPart("arquivo", new File("src/main/resources/users.pdf"))
+
+                .when()
+                    .post("https://restapi.wcaquino.me/upload")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void deveOFazerDownloadArquivo() throws IOException {
+
+        byte[] image = given().log().all()
+                .when()
+                    .get("https://restapi.wcaquino.me/download")
+                .then()
+                    .statusCode(200)
+                    .extract().asByteArray();
+
+        File imagem = new File("src/main/resources/file.jpg");
+        OutputStream out = new FileOutputStream(imagem);
+        out.write(image);
+        out.close();
+        Assert.assertThat(imagem.length(), lessThan(100000L));
+    }
+
+}
